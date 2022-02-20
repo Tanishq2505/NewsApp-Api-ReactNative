7f179c8fe91f6434980abf583303b2267b7fc9f4
Tanishq Kashyap <tanishqa89@gmail.com>
2022-02-20 22:35:27 +0530

    added feature to bookmark screen

--- Screens/BookmarkScreen.js
+++ Screens/BookmarkScreen.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, Button, TouchableOpacityBase, TouchableOpacity } from 'react-native'
 
 const BookmarkScreen = () => {
+    const [loggedIn, setLoggedIn] = useState(0)
+
+    function loginCheck(){
+        return 0
+    }
+    function login(){
+        if(loggedIn){
+            setLoggedIn(0)
+        }else setLoggedIn(1)
+    }
+    loginCheck();
     return (
         <View>
-            <Text>Bookmarks</Text>
+            {!loggedIn?(
+                <View>
+                <Text>Hello 1</Text>
+                <TouchableOpacity onPress={()=>login()}>
+                    <Text>Login</Text>
+                </TouchableOpacity>
+                </View>
+            ):(
+                <View>
+                <Text>Hello 2</Text>
+                <TouchableOpacity onPress={()=>login()}>
+                    <Text>SignOut</Text>
+                </TouchableOpacity>
+                </View>
+            )}
         </View>
     )
 }
